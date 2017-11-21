8fa83aac8c1753ddc25f28ccd20872fe35f4b7e9
Alex Zalizniak <alex@xplace.com>
2017-11-21 14:09:33 +0200

    Version print

--- src/main/resources/static/js/ignitionFrontDas.js
+++ src/main/resources/static/js/ignitionFrontDas.js
@@ -1,7 +1,29 @@
 /**
  *
  */
-IGNITION_FRONT_APP.service('ignitionFrontDas', ['$rootScope', function ($rootScope) {
+IGNITION_FRONT_APP.service('ignitionFrontDas', ['$rootScope', '$http', '$q', function ($rootScope, $http, $q) {
     var srv = this;
+
+
+    srv.getIgnitionServerVersion = function () {
+        var deferred = $q.defer();
+        var promise = $http.get('/ignition/rest/about');
+        promise.then(function (response) {
+            var AboutIgnitionDto = response.data;
+            deferred.resolve(mapAboutIgnitionDto(AboutIgnitionDto));
+        });
+        return deferred.promise;
+    };
+
+
+    //
+    //
+    //
+
+    function mapAboutIgnitionDto(AboutIgnitionDto) {
+        return {
+            ignitionVersion: AboutIgnitionDto['version']
+        }
+    }
 
 }]);
--- src/main/resources/static/js/ignitionFrontMainCtrl.js
+++ src/main/resources/static/js/ignitionFrontMainCtrl.js
@@ -4,8 +4,18 @@
 IGNITION_FRONT_APP.controller('ignitionFrontMainCtrl', ['$scope', '$timeout', 'ignitionFrontDas', function ($scope, $timeout, ignitionFrontDas) {
     var vm = this;
 
-    vm.sayIgnitionHello = function () {
-        console.info("Hello..");
+    vm.initIgnitionFront = function () {
+        init();
+    };
+
+    //
+    //
+    //
+
+    function init() {
+        ignitionFrontDas.getIgnitionServerVersion().then(function (mappedAboutIgnitionDto) {
+            console.info("Server version: " + mappedAboutIgnitionDto.ignitionVersion);
+        });
     }
 
 }]);
