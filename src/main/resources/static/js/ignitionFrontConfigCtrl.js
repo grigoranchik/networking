969e8cb1cd2c72b213ad890d2a425161968014a4
Alex Zalizniak <alex@xplace.com>
2017-11-24 10:41:20 +0200

    Play config

---
+++ src/main/resources/static/js/ignitionFrontConfigCtrl.js
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+IGNITION_FRONT_APP.controller('ignitionFrontConfigCtrl', ['$scope', '$timeout', 'ignitionFrontDas', function ($scope, $timeout, ignitionFrontDas) {
+    var vm = this;
+
+
+}]);
