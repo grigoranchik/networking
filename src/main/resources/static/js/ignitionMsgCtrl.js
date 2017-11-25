f7915a674b38818c71853ffa004811f068d966e5
Alex Zalizniak <alex@xplace.com>
2017-11-25 11:56:05 +0200

    Messages - ui

--- src/main/resources/static/js/ignitionMsgCtrl.js
+++ src/main/resources/static/js/ignitionMsgCtrl.js
@@ -2,7 +2,6 @@
  *
  */
 IGNITION_FRONT_APP.controller('ignitionMsgCtrl', ['$scope', '$timeout', '$http', '$q', function ($scope, $timeout, $http, $q) {
-    var vm = this;
 
     $scope.allChatMessages = [];
 
@@ -12,17 +11,9 @@ IGNITION_FRONT_APP.controller('ignitionMsgCtrl', ['$scope', '$timeout', '$http',
         sendMessageToChat($scope.newMessageText)
     };
 
-    getChatMesssages();
-
-    //
-    //
-    //
-
-
-    function getChatMesssages() {
+    function getChatMessages() {
         var promise = $http.get('https://176.36.229.152/ignition/rest/messages/list');
         promise.then(function (response) {
-            console.info("All messages: " + response.data);
             $scope.allChatMessages = response.data['availableMessagesList'];
         });
     }
@@ -30,9 +21,10 @@ IGNITION_FRONT_APP.controller('ignitionMsgCtrl', ['$scope', '$timeout', '$http',
     function sendMessageToChat(messageText) {
         var promise = $http.post('https://176.36.229.152/ignition/rest/messages/send', {newMessageText: messageText}, {});
         promise.then(function (response) {
-            console.info("Message sending response: " + response.data);
-            getChatMesssages();
+            getChatMessages();
         });
     }
+
+    getChatMessages();
 
 }]);
