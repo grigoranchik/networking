84b3717667dec065d32754af1d6878afd2f6da8f
Alex Zalizniak <alex@xplace.com>
2017-11-25 11:47:36 +0200

    Messages - ui

--- src/main/resources/static/js/ignitionMsgCtrl.js
+++ src/main/resources/static/js/ignitionMsgCtrl.js
@@ -4,27 +4,34 @@
 IGNITION_FRONT_APP.controller('ignitionMsgCtrl', ['$scope', '$timeout', '$http', '$q', function ($scope, $timeout, $http, $q) {
     var vm = this;
 
+    $scope.allChatMessages = [];
+
     $scope.newMessageText = null;
 
     $scope.onSendNewMessageClicked = function () {
-        sendMessageToChat(  $scope.newMessageText )
+        sendMessageToChat($scope.newMessageText)
     };
+
+    getChatMesssages();
 
     //
     //
     //
+
 
     function getChatMesssages() {
         var promise = $http.get('https://176.36.229.152/ignition/rest/messages/list');
         promise.then(function (response) {
             console.info("All messages: " + response.data);
+            $scope.allChatMessages = response.data['availableMessagesList'];
         });
     }
 
     function sendMessageToChat(messageText) {
-        var promise = $http.post('https://176.36.229.152/ignition/rest/messages/send');
+        var promise = $http.post('https://176.36.229.152/ignition/rest/messages/send', {newMessageText: messageText}, {});
         promise.then(function (response) {
             console.info("Message sending response: " + response.data);
+            getChatMesssages();
         });
     }
 
