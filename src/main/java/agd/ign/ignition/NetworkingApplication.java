0fcbe00fd332f6a95d38971e6a6b52a24a9214b2
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-12-09 16:07:38 +0200

    Cleaned up

--- src/main/java/agd/ign/ignition/NetworkingApplication.java
+++ src/main/java/agd/ign/ignition/NetworkingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IgnitionApplication {
+public class NetworkingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IgnitionApplication.class, args);
+		SpringApplication.run(NetworkingApplication.class, args);
 	}
 }
